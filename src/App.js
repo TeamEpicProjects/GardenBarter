1347e86f6a8cc20703523627f54d924f9be42e4d
apm191 <apurv.mundhra@gmail.com>
2022-03-14 18:07:39 +0530

    routing done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
-import LandingPage from './components/LandingPage';
+import Login from './login'
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+import Dashboard from './components/Dashboard'
+
 function App() {
   return (
     <div className="App">
-      <LandingPage />
+      {/* navbar */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- src/components/Dashboard/index.jsx
+++ src/components/Dashboard/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import '../css/dashboard.css'
 
 const Dashboard = () => {
     return (
