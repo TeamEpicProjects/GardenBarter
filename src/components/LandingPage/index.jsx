229ce63ece5a41fe17bbecce12c8deb399e048cf
apm191 <apurv.mundhra@gmail.com>
2022-03-14 16:44:02 +0530

    first commit

---
+++ src/components/LandingPage/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Signin from './Signin'
+import Register from './Register'
+import Register from './Navbar'
+const LandingPage = () => {
+
+    return (
+        <div>
+            <Navbar />
+            <Signin />
+            <Register />
+        </div>
+    )
+}
+
+export default LandingPage
---
+++ src/components/LandingPage/navbar.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+const Navbar = () => {
+    return (
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="" alt="" width="30" height="24" class="d-inline-block align-text-top" />GardenBarter
+                </a>
+            </div>
+        </nav>
+    )
+}
+export default Navbar
